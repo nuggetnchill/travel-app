cf0f3c59c0ad2e1beccf0a52c6a33eda07e7304f
Justin Nguyen <nuggetnchill@gmail.com>
2020-11-12 13:21:39 -0600

    added addMarkerPopup for adding newEntry through component LogEntryForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+
 import { fetchLogEntries } from './API';
+import LogEntryForm from './LogEntryForm';
 
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
   const [showPopup, setShowPopup] = useState({});
+  const [newEntry, setNewEntry] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -13,12 +16,23 @@ const App = () => {
     zoom: 3,
   });
 
+  const getEntries = async () => {
+    const logEntries = await fetchLogEntries();
+    setLogEntries(logEntries);
+  };
+
   useEffect(() => {
-    (async () => {
-      const logEntries = await fetchLogEntries();
-      setLogEntries(logEntries);
-    })();
+    getEntries();
   }, []);
+
+  //double click on map
+  const addMarkerPopup = (event) => {
+    const [longitude, latitude] = event.lngLat;
+    setNewEntry({
+      latitude,
+      longitude,
+    });
+  };
 
   return (
     <ReactMapGL
@@ -26,6 +40,7 @@ const App = () => {
       mapStyle='mapbox://styles/nuggetnchill/ckhcmthgi0n1419o5raelji1w'
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={setViewport}
+      onDblClick={addMarkerPopup}
     >
       {logEntries.map((entry) => (
         <>
@@ -35,15 +50,28 @@ const App = () => {
             longitude={entry.longitude}
           >
             <div onClick={() => setShowPopup({ [entry._id]: true })}>
-              <img
-                className='marker'
+              <svg
+                className='marker yellow'
                 style={{
-                  height: `${5 * viewport.zoom}px`,
-                  width: `${5 * viewport.zoom}px`,
+                  height: `${6 * viewport.zoom}px`,
+                  width: `${6 * viewport.zoom}px`,
                 }}
-                src='https://i.imgur.com/y0G5YTX.png'
-                alt='marker'
-              />
+                version='1.1'
+                id='Layer_1'
+                x='0px'
+                y='0px'
+                viewBox='0 0 512 512'
+              >
+                <g>
+                  <g>
+                    <path
+                      d='M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                      c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                      c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z'
+                    />
+                  </g>
+                </g>
+              </svg>
             </div>
           </Marker>
           {showPopup[entry._id] ? (
@@ -54,6 +82,7 @@ const App = () => {
               closeOnClick={false}
               dynamicPosition={true}
               onClose={() => setShowPopup({})}
+              anchor='top'
             >
               <div className='popup'>
                 <h3>{entry.title}</h3>
@@ -66,6 +95,49 @@ const App = () => {
           ) : null}
         </>
       ))}
+      {newEntry ? (
+        <>
+          <Marker latitude={newEntry.latitude} longitude={newEntry.longitude}>
+            <div>
+              <svg
+                className='marker red'
+                style={{
+                  height: `${6 * viewport.zoom}px`,
+                  width: `${6 * viewport.zoom}px`,
+                }}
+                version='1.1'
+                id='Layer_1'
+                x='0px'
+                y='0px'
+                viewBox='0 0 512 512'
+              >
+                <g>
+                  <g>
+                    <path
+                      d='M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                      c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                      c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z'
+                    />
+                  </g>
+                </g>
+              </svg>
+            </div>
+          </Marker>
+          <Popup
+            latitude={newEntry.latitude}
+            longitude={newEntry.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            dynamicPosition={true}
+            onClose={() => setNewEntry(null)}
+            anchor='top'
+          >
+            <div className='popup'>
+              <LogEntryForm />
+            </div>
+          </Popup>
+        </>
+      ) : null}
     </ReactMapGL>
   );
 };
